444f45737e27c1f6494caf43ce87afe04b3ff27c
programistro <r.grigorev1502@gmail.com>
2026-01-08 01:11:26 +0300

    Добавил вращение камеры Rotate по игроку

--- Source/Game/FreeCamera.cs
+++ Source/Game/FreeCamera.cs
@@ -10,6 +10,12 @@ public class FreeCamera : Script
 
     private float pitch;
     private float yaw;
+    public CursorLockMode mode;
+    // Player
+    public Actor Target;
+    public float MinDistance = 200;
+    public float MaxDistance = 1500;
+    public float ZoomSpeed = 500;
 
     public override void OnStart()
     {
@@ -20,29 +26,73 @@ public class FreeCamera : Script
 
     public override void OnUpdate()
     {
-        Screen.CursorVisible = false;
-        Screen.CursorLock = CursorLockMode.Locked;
+        Screen.CursorVisible = true;
+        // Screen.CursorLock = CursorLockMode.Clipped;
+        Screen.CursorLock = mode;
 
-        var mouseDelta = new Float2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        pitch = Mathf.Clamp(pitch + mouseDelta.Y, -88, 88);
-        yaw += mouseDelta.X;
+        // var mouseDelta = new Float2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        // pitch = Mathf.Clamp(pitch + mouseDelta.Y, -88, 88);
+        // yaw += mouseDelta.X;
     }
 
     public override void OnFixedUpdate()
     {
-        var camTrans = Actor.Transform;
-        var camFactor = Mathf.Saturate(CameraSmoothing * Time.DeltaTime);
+        // направляме камеру на игрока
+        Actor.LookAt(Target.Position, Vector3.Up);
+        Debug.Log(Vector3.Up + "up");
+        if (Input.Mouse.GetButton(MouseButton.Middle))
+        {
+            var camTrans = Actor.Transform;
+            var camFactor = Mathf.Saturate(CameraSmoothing * Time.DeltaTime);
 
-        camTrans.Orientation = Quaternion.Lerp(camTrans.Orientation, Quaternion.Euler(pitch, yaw, 0), camFactor);
+            // camTrans.Orientation = Quaternion.Lerp(camTrans.Orientation, Quaternion.Euler(pitch, yaw, 0), camFactor);
 
-        var inputH = Input.GetAxis("Horizontal");
-        var inputV = Input.GetAxis("Vertical");
-        var move = new Vector3(inputH, 0.0f, inputV);
-        move.Normalize();
-        move = camTrans.TransformDirection(move);
+            var inputH = Input.GetAxis("Horizontal");
+            var inputV = Input.GetAxis("Vertical");
+            var move = new Vector3(inputH, 0.0f, inputV);
+            move.Normalize();
+            move = camTrans.TransformDirection(move);
 
-        camTrans.Translation += move * MoveSpeed;
+            camTrans.Translation += move * MoveSpeed;
+            Actor.Transform = camTrans;
+        }
+        
+        // var desiredPos = Target.Position + Offset;
+        //
+        // // Двигаем камеру (можно сразу, можно через lerp для плавности)
+        // Actor.Position = Float3.Lerp(Actor.Position, desiredPos, 10.0f * Time.DeltaTime);
+        //
+        // // Поворачиваем камеру в сторону игрока
+        // Actor.Orientation = Quaternion.LookAt(Actor.Position, Target.Position);
 
-        Actor.Transform = camTrans;
+        var scroll = Input.Mouse.ScrollDelta;
+        if (scroll != 0)
+        {
+            // Debug.Log($"scroll {inp}");
+            // var camTrans = Actor.Transform;
+            // // var camFactor = Mathf.Saturate(CameraSmoothing * Time.DeltaTime);
+            //
+            // // camTrans.Orientation = Quaternion.Lerp(camTrans.Orientation, Quaternion.Euler(pitch, yaw, 0), camFactor);
+            // var move = new Vector3(inp, inp, inp);
+            // move.Normalize();
+            // move = camTrans.TransformDirection(move);
+            //
+            // camTrans.Translation += move * MoveSpeed;
+            //
+            // Actor.Transform = camTrans;
+            var dir = Target.Position - Actor.Position;
+            var distance = dir.Length;
+            
+            if (distance <= 0.001f)
+                return;
+            dir /= distance;
+            
+            var delta = scroll * ZoomSpeed * Time.DeltaTime;
+            
+            var newDistance = Mathf.Clamp(distance - delta, MinDistance, MaxDistance);
+            
+            Actor.Position = Target.Position - dir * newDistance;
+            Debug.Log(Target.Position - dir * newDistance);
+        }
     }
 }
---
+++ Source/Game/Player/Player.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// Player Script.
+/// </summary>
+public class Player : Script
+{
+    [Limit(0, 100), Tooltip("Player movement speed factor")]
+    public float MoveSpeed { get; set; } = 4;
+
+    public RigidBody RigidBody;
+
+    public Prefab Prefab;
+    
+    /// <inheritdoc/>
+    public override void OnStart()
+    {
+        // Here you can add code that needs to be called when script is created, just before the first game update
+        
+        RayCastHit hit;
+        if (Physics.RayCast(Actor.Position, Actor.Direction, out hit))
+        {
+            DebugDraw.DrawSphere(new BoundingSphere(hit.Point, 50), Color.Red);
+        }
+    }
+    
+    /// <inheritdoc/>
+    public override void OnEnable()
+    {
+        // Here you can add code that needs to be called when script is enabled (eg. register for events)
+    }
+
+    /// <inheritdoc/>
+    public override void OnDisable()
+    {
+        // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
+    }
+
+    /// <inheritdoc/>
+    public override void OnUpdate()
+    {
+        // Here you can add code that needs to be called every frame
+    }
+    
+    private Vector3 _velocity;
+    public CharacterController _controller;
+
+    public override void OnFixedUpdate()
+    {
+        var inputX = Input.GetAxis("Horizontal");
+        var inputY = Input.GetAxis("Vertical");
+        
+        Vector3 move = new  Vector3(inputX, 0, inputY);
+        
+        move.Normalize();
+        
+        if (move != Vector3.Zero)
+        {
+            RigidBody.AddMovement(move);
+            Debug.Log("Moving " + move);
+        }
+        
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetMouseButtonDown((MouseButton.Left)))
+        {
+            if (!Input.GetMouseButtonDown(MouseButton.Left))
+                return;
+            var pos = Input.MousePosition;
+            var ray = Camera.MainCamera.ConvertMouseToRay(pos);
+            if (!Physics.RayCast(ray.Position, ray.Direction, out var hit))
+                return;
+               
+            DebugDraw.DrawSphere(new BoundingSphere(hit.Point, 50), Color.Red);
+            PrefabManager.SpawnPrefab(Prefab, hit.Point);
+        }
+    }
+}
