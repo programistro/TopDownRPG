63088f6c4afe28942aa25ed801c2ae51bde4718a
programistro <r.grigorev1502@gmail.com>
2026-01-09 00:05:28 +0300

    сделал вращеине камеры по оси y

--- Source/Game/FreeCamera.cs
+++ Source/Game/FreeCamera.cs
@@ -22,64 +22,54 @@ public class FreeCamera : Script
         var initialEulerAngles = Actor.Orientation.EulerAngles;
         pitch = initialEulerAngles.X;
         yaw = initialEulerAngles.Y;
+
+        Actor.LookAt(Target.Position, Vector3.Up);
     }
 
     public override void OnUpdate()
     {
         Screen.CursorVisible = true;
-        // Screen.CursorLock = CursorLockMode.Clipped;
         Screen.CursorLock = mode;
 
-        // var mouseDelta = new Float2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        // pitch = Mathf.Clamp(pitch + mouseDelta.Y, -88, 88);
-        // yaw += mouseDelta.X;
+        var mouseDelta = new Float2(Input.GetAxis("Mouse X"), Target.Position.Z);
+        pitch = Mathf.Clamp(pitch + mouseDelta.Y, -88, 88);
+        yaw += mouseDelta.X;
     }
 
     public override void OnFixedUpdate()
     {
-        // направляме камеру на игрока
-        Actor.LookAt(Target.Position, Vector3.Up);
+        // Actor.LookAt(Target.Position, Vector3.Up);
+        
+        var inputX = Input.GetAxis("Horizontal");
+        var inputY = Input.GetAxis("Vertical");
+        
+        Vector3 move = new  Vector3(inputX, 0, inputY);
+        
+        move.Normalize();
+        
+        if (move != Vector3.Zero)
+        {
+            Transform.TransformDirection(move);
+            Debug.Log("Moving " + move);
+        }
+        
         Debug.Log(Vector3.Up + "up");
         if (Input.Mouse.GetButton(MouseButton.Middle))
         {
             var camTrans = Actor.Transform;
-            var camFactor = Mathf.Saturate(CameraSmoothing * Time.DeltaTime);
-
-            // camTrans.Orientation = Quaternion.Lerp(camTrans.Orientation, Quaternion.Euler(pitch, yaw, 0), camFactor);
-
-            var inputH = Input.GetAxis("Horizontal");
-            var inputV = Input.GetAxis("Vertical");
-            var move = new Vector3(inputH, 0.0f, inputV);
             move.Normalize();
             move = camTrans.TransformDirection(move);
-
+            
             camTrans.Translation += move * MoveSpeed;
+            var camFactor = Mathf.Saturate(CameraSmoothing * Time.DeltaTime);
+            Debug.Log($"camFactor: {camFactor}");
+            camTrans.Orientation = Quaternion.Lerp(camTrans.Orientation, Quaternion.Euler(pitch, yaw, 0), camFactor);
             Actor.Transform = camTrans;
         }
-        
-        // var desiredPos = Target.Position + Offset;
-        //
-        // // Двигаем камеру (можно сразу, можно через lerp для плавности)
-        // Actor.Position = Float3.Lerp(Actor.Position, desiredPos, 10.0f * Time.DeltaTime);
-        //
-        // // Поворачиваем камеру в сторону игрока
-        // Actor.Orientation = Quaternion.LookAt(Actor.Position, Target.Position);
 
         var scroll = Input.Mouse.ScrollDelta;
         if (scroll != 0)
         {
-            // Debug.Log($"scroll {inp}");
-            // var camTrans = Actor.Transform;
-            // // var camFactor = Mathf.Saturate(CameraSmoothing * Time.DeltaTime);
-            //
-            // // camTrans.Orientation = Quaternion.Lerp(camTrans.Orientation, Quaternion.Euler(pitch, yaw, 0), camFactor);
-            // var move = new Vector3(inp, inp, inp);
-            // move.Normalize();
-            // move = camTrans.TransformDirection(move);
-            //
-            // camTrans.Translation += move * MoveSpeed;
-            //
-            // Actor.Transform = camTrans;
             var dir = Target.Position - Actor.Position;
             var distance = dir.Length;
             
@@ -96,3 +86,5 @@ public class FreeCamera : Script
         }
     }
 }
+
+//todo сделать Lock (bool) систему, когда игрок кликнул на иконку персонажа камера вращалась на игрока, а когда нет то свободаня камера
